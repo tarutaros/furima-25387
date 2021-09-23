a42562e8ec89958af48d921d4f49cd28996c7857
hoshin <66007415+tarutaros@users.noreply.github.com>
2021-09-23 16:17:01 +0900

    ユーザー管理機能訂正２回目

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -10,31 +10,6 @@ RSpec.describe User, type: :model do
       it 'nicknameとemai,passwordとpassword_confirramation,family_name,first_name,family_name_kana,first_name_kana,birthdayが存在すれば登録できる'do
         expect(@user).to be_valid
       end
-      # it 'passwordが6文字以上の半角英数字混合であれば登録できる' do
-      #   @user.password = "000aaa"
-      #   @user.password_confirmation = "000aaa"
-      #   expect(@user).to be_valid
-      # end
-      # it 'family_nameが全角文字であれば登録できる' do
-      #   @user.family_name = "山田"
-      #   expect(@user).to be_valid
-      # end
-      # it 'first_nameが全角文字であれば登録できる' do
-      #   @user.family_name = "太郎"
-      #   expect(@user).to be_valid
-      # end
-      # it 'family_name_kanaが全角文字であれば登録できる' do
-      #   @user.family_name_kana = "ヤマダ"
-      #   expect(@user).to be_valid
-      # end
-      # it 'first_name_kanaが全角文字であれば登録できる' do
-      #   @user.first_name_kana = "タロウ"
-      #   expect(@user).to be_valid
-      # end
-      # it 'birthdayが入力されていれば登録できる' do
-      #   @user.birthday = "1950-01-23"
-      #   expect(@user).to be_valid
-      # end
     end
     context '新規登録できないとき' do
       it "nicknameが空では登録できない" do

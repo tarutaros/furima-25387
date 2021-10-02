e575eff34aa1312492a5b103511a2b99858d39c2
hoshin <66007415+tarutaros@users.noreply.github.com>
2021-10-03 06:27:50 +0900

    商品詳細表示機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user! , except: :index
+  before_action :authenticate_user! , except: [:index, :show]
 
   def index
     @items = Item.includes(:user).order("created_at  DESC")
@@ -16,6 +16,15 @@ class ItemsController < ApplicationController
     else
       render :new
     end
+  end
+
+  # def edit
+  #   @item = Item.find_by(id: params[:id])
+  # end
+
+  def show
+    @item =Item.find_by(id: params[:id])
+
   end
 
   private
